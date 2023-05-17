a1d0e3e6d72ce276eba033c43f5a0dc35fed2d33
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-05-17 14:23:11 +0600

    Added the boilerplate code for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blogs from './pages/Blogs';
+import Portfolio from './pages/Portfolio';
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <nav className="navbar">
+                <ul className="menu">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/blogs">Blogs</Link>
+                    </li>
+                    <li>
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/"
+                       element={<Home/>}/>
+                <Route path="/about"
+                       element={<About/>}/>
+                <Route path="/blogs"
+                       element={<Blogs/>}/>
+                <Route path="/portfolio"
+                       element={<Portfolio/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Blogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h1>Blogs Page</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default Blogs;
